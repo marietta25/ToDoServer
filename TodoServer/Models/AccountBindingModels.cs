e683065045aebf62e5beb44c6696437f45ecffc2
marietta25 <32923340+marietta25@users.noreply.github.com>
2019-02-05 13:59:13 +0200

    Update RegisterBindingModel

--- TodoServer/Models/AccountBindingModels.cs
+++ TodoServer/Models/AccountBindingModels.cs
@@ -37,6 +37,14 @@ namespace TodoServer.Models
         [Required]
         [Display(Name = "Email")]
         public string Email { get; set; }
+        
+        [Required]
+        [Display(Name = "First Name")]
+        public string Firstname { get; set; }
+        
+        [Required]
+        [Display(Name = "Last Name")]
+        public string Lastname { get; set; }
 
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
