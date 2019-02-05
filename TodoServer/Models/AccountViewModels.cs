df38c30f2b6a9391beb96773e81bfbc232d965dc
marietta25 <32923340+marietta25@users.noreply.github.com>
2019-02-05 14:31:48 +0200

    Get user name from model

--- TodoServer/Models/AccountViewModels.cs
+++ TodoServer/Models/AccountViewModels.cs
@@ -28,6 +28,10 @@ namespace TodoServer.Models
     public class UserInfoViewModel
     {
         public string Email { get; set; }
+        
+        public string Firstname { get; set; }
+        
+        public string Lastname { get; set; }
 
         public bool HasRegistered { get; set; }
 
